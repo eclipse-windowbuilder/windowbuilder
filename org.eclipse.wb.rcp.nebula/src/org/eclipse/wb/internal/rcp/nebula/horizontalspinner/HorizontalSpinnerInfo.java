b8d445558f9ff7f5a69cd28d28a42f23dd3358bd
Laurent CARON <laurent.caron@gmail.com>
2020-04-19 12:38:43 +0200

    Bug 561298 - How to integrate Nebula widgets

---
+++ org.eclipse.wb.rcp.nebula/src/org/eclipse/wb/internal/rcp/nebula/horizontalspinner/HorizontalSpinnerInfo.java
@@ -0,0 +1,25 @@
+package org.eclipse.wb.internal.rcp.nebula.horizontalspinner;
+
+import org.eclipse.wb.internal.core.model.creation.CreationSupport;
+import org.eclipse.wb.internal.core.model.description.ComponentDescription;
+import org.eclipse.wb.internal.core.model.property.editor.complex.InstanceObjectPropertyEditor;
+import org.eclipse.wb.internal.core.utils.ast.AstEditor;
+import org.eclipse.wb.internal.swt.model.widgets.CompositeInfo;
+
+/**
+ * Model for {@link HorizontalSpinner}.
+ */
+public class HorizontalSpinnerInfo extends CompositeInfo {
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  // Constructor
+  //
+  ////////////////////////////////////////////////////////////////////////////
+  public HorizontalSpinnerInfo(AstEditor editor,
+      ComponentDescription description,
+      CreationSupport creationSupport) throws Exception {
+    super(editor, description, creationSupport);
+    // listener for setting property to default
+    InstanceObjectPropertyEditor.installListenerForProperty(this);
+  }
+}
