aff8025a5ce2ad63ef7db9efb4a947e82069baaf
Marcel du Preez <marcel.dupreez@s4.co.za>
2021-11-24 11:38:51 +0200

    Adding a public getViewer method
    We need the possibility of obtaining this GraphicalViewer object in our
    code. This is so we can use it in our own application specific drag and
    drop implementation.
    Update to contributors
    
    Updated the last date changed within the copyright

--- org.eclipse.wb.core/src/org/eclipse/wb/internal/core/editor/DesignComposite.java
+++ org.eclipse.wb.core/src/org/eclipse/wb/internal/core/editor/DesignComposite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Google, Inc.
+ * Copyright (c) 2011, 2021 Google, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Google, Inc. - initial API and implementation
+ *    Marcel du Preez - GraphicalViewer getter method
  *******************************************************************************/
 package org.eclipse.wb.internal.core.editor;
 
@@ -115,7 +116,10 @@ public abstract class DesignComposite extends Composite {
   protected void createGEFComposite(Composite parent) {
     PluginFlyoutPreferences preferences =
         new PluginFlyoutPreferences(DesignerPlugin.getPreferences(), "design.palette");
-    preferences.initializeDefaults(IFlyoutPreferences.DOCK_WEST, IFlyoutPreferences.STATE_OPEN, 210);
+    preferences.initializeDefaults(
+        IFlyoutPreferences.DOCK_WEST,
+        IFlyoutPreferences.STATE_OPEN,
+        210);
     FlyoutControlComposite gefComposite = new FlyoutControlComposite(parent, SWT.NONE, preferences);
     GridDataFactory.create(gefComposite).grab().fill();
     gefComposite.setTitleText("Palette");
@@ -233,16 +237,19 @@ public abstract class DesignComposite extends Composite {
     // IExtractableControl
     //
     ////////////////////////////////////////////////////////////////////////////
+    @Override
     public Control getControl() {
       return m_control;
     }
 
+    @Override
     public void extract(Composite newParent) {
       m_control.setParent(newParent);
       doLayout(m_oldParent);
       newParent.layout();
     }
 
+    @Override
     public void restore() {
       m_control.setParent(m_oldParent);
       m_control.setVisible(true);
@@ -276,4 +283,13 @@ public abstract class DesignComposite extends Composite {
    * @return the {@link IExtractableControl} for accessing "Structure" {@link Control}.
    */
   public abstract IExtractableControl getExtractablePalette();
+
+  /**
+   * Get the graphical viewer of this composite.
+   *
+   * @return
+   */
+  public GraphicalViewer getViewer() {
+    return m_viewer;
+  }
 }
