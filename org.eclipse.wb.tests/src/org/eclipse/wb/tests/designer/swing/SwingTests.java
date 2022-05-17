73993e7b8488aec737e05e3c18dc572a3467ad7f
Lars Vogel <Lars.Vogel@vogella.com>
2022-05-17 15:13:49 +0200

    Moving SwingTests to JUnit 4

--- org.eclipse.wb.tests/src/org/eclipse/wb/tests/designer/swing/SwingTests.java
+++ org.eclipse.wb.tests/src/org/eclipse/wb/tests/designer/swing/SwingTests.java
@@ -10,30 +10,29 @@
  *******************************************************************************/
 package org.eclipse.wb.tests.designer.swing;
 
-import org.eclipse.wb.tests.designer.core.DesignerSuiteTests;
 import org.eclipse.wb.tests.designer.swing.ams.AmsTests;
 import org.eclipse.wb.tests.designer.swing.jsr296.ApplicationFrameworkTests;
 import org.eclipse.wb.tests.designer.swing.laf.LookAndFeelTest;
 import org.eclipse.wb.tests.designer.swing.model.ModelTests;
 import org.eclipse.wb.tests.designer.swing.swingx.SwingXTests;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
 /**
  * @author scheglov_ke
  */
-public class SwingTests extends DesignerSuiteTests {
-  public static Test suite() {
-    TestSuite suite = new TestSuite("org.eclipse.wb.swing");
-    suite.addTest(createSingleSuite(LookAndFeelTest.class));
-    suite.addTest(createSingleSuite(ConvertersTest.class));
-    suite.addTest(createSingleSuite(CustomizeTest.class));
-    suite.addTest(ModelTests.suite());
-    suite.addTest(AmsTests.suite());
-    suite.addTest(SwingXTests.suite());
-    suite.addTest(ApplicationFrameworkTests.suite());
-    //suite.addTest(createSingleSuite(WaitForMemoryProfilerTest.class));
-    return suite;
-  }
+@RunWith(Suite.class)
+@SuiteClasses({
+    LookAndFeelTest.class,
+    ConvertersTest.class,
+    CustomizeTest.class,
+    ModelTests.class,
+    AmsTests.class,
+    SwingXTests.class,
+    ApplicationFrameworkTests.class,
+//  WaitForMemoryProfilerTest.class,
+})
+public class SwingTests {
 }
