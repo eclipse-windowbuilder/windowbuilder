ea8206775b82c48ff5ddff160ff8e3d5f12b0b46
Lars Vogel <Lars.Vogel@vogella.com>
2022-06-21 16:12:31 +0200

    Convert Swing2SwtTests to JUnit4 test suite

--- org.eclipse.wb.tests/src/org/eclipse/wb/tests/designer/rcp/swing2swt/Swing2SwtTests.java
+++ org.eclipse.wb.tests/src/org/eclipse/wb/tests/designer/rcp/swing2swt/Swing2SwtTests.java
@@ -10,22 +10,21 @@
  *******************************************************************************/
 package org.eclipse.wb.tests.designer.rcp.swing2swt;
 
-import org.eclipse.wb.tests.designer.core.DesignerSuiteTests;
-
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
 /**
  * Tests for Swing2SWT.
  *
  * @author scheglov_ke
  */
-public class Swing2SwtTests extends DesignerSuiteTests {
-  public static Test suite() {
-    TestSuite suite = new TestSuite("org.eclipse.wb.rcp.Swing2SWT");
-    suite.addTest(createSingleSuite(BoxLayoutTest.class));
-    suite.addTest(createSingleSuite(FlowLayoutTest.class));
-    suite.addTest(createSingleSuite(BorderLayoutTest.class));
-    return suite;
-  }
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	BoxLayoutTest.class,
+	FlowLayoutTest.class,
+	BorderLayoutTest.class,
+})
+public class Swing2SwtTests {
 }
