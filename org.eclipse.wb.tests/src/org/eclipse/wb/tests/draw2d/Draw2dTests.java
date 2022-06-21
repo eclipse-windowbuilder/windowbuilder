baf98481add8d67a4ec1c6a774496761436de6d2
Lars Vogel <Lars.Vogel@vogella.com>
2022-06-21 16:15:04 +0200

    Convert Draw2dTests to JUnit4 test suite

--- org.eclipse.wb.tests/src/org/eclipse/wb/tests/draw2d/Draw2dTests.java
+++ org.eclipse.wb.tests/src/org/eclipse/wb/tests/draw2d/Draw2dTests.java
@@ -10,38 +10,39 @@
  *******************************************************************************/
 package org.eclipse.wb.tests.draw2d;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
 /**
  * @author lobas_av
  */
-public class Draw2dTests extends TestCase {
-  public static Test suite() {
-    TestSuite suite = new TestSuite("org.eclipse.wb.draw2d");
-    // geometry
-    suite.addTestSuite(IntervalTest.class);
-    suite.addTestSuite(PointTest.class);
-    suite.addTestSuite(InsetsTest.class);
-    suite.addTestSuite(DimensionTest.class);
-    suite.addTestSuite(RectangleTest.class);
-    suite.addTestSuite(PointListTest.class);
-    suite.addTestSuite(TransposerTest.class);
+
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	 // geometry
+    IntervalTest.class,
+    PointTest.class,
+    InsetsTest.class,
+    DimensionTest.class,
+    RectangleTest.class,
+    PointListTest.class,
+    TransposerTest.class,
     // Border
-    suite.addTestSuite(MarginBorderTest.class);
-    suite.addTestSuite(LineBorderTest.class);
-    suite.addTestSuite(CompoundBorderTest.class);
+    MarginBorderTest.class,
+    LineBorderTest.class,
+    CompoundBorderTest.class,
     // Figure
-    suite.addTestSuite(GraphicsTest.class);
-    suite.addTestSuite(FigureTest.class);
-    suite.addTestSuite(FigurePaintingTest.class);
-    suite.addTestSuite(LabelTest.class);
-    suite.addTestSuite(PolylineTest.class);
-    suite.addTestSuite(LayerTest.class);
-    suite.addTestSuite(RootFigureTest.class);
+    GraphicsTest.class,
+    FigureTest.class,
+    FigurePaintingTest.class,
+    LabelTest.class,
+    PolylineTest.class,
+    LayerTest.class,
+    RootFigureTest.class,
     // events
-    suite.addTestSuite(FigureEventTest.class);
-    return suite;
-  }
+    FigureEventTest.class,
+})
+public class Draw2dTests {
 }
