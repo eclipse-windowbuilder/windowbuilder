4437f1fb4b071f1b1f612ab342c53ec1d9dd69ea
Lars Vogel <Lars.Vogel@vogella.com>
2022-06-21 16:03:39 +0200

    Convert ModelTests to JUnit4 test suite

--- org.eclipse.wb.tests/src/org/eclipse/wb/tests/designer/rcp/model/ModelTests.java
+++ org.eclipse.wb.tests/src/org/eclipse/wb/tests/designer/rcp/model/ModelTests.java
@@ -10,35 +10,36 @@
  *******************************************************************************/
 package org.eclipse.wb.tests.designer.rcp.model;
 
-import org.eclipse.wb.tests.designer.core.DesignerSuiteTests;
 import org.eclipse.wb.tests.designer.rcp.model.e4.E4Tests;
 import org.eclipse.wb.tests.designer.rcp.model.forms.FormsTests;
 import org.eclipse.wb.tests.designer.rcp.model.jface.JFaceTests;
 import org.eclipse.wb.tests.designer.rcp.model.layout.LayoutTests;
-import org.eclipse.wb.tests.designer.rcp.model.property.PropertyTests;
 import org.eclipse.wb.tests.designer.rcp.model.rcp.TheRcpTests;
 import org.eclipse.wb.tests.designer.rcp.model.util.UtilTests;
 import org.eclipse.wb.tests.designer.rcp.model.widgets.WidgetTests;
+import org.eclipse.wb.tests.designer.rcp.resource.ResourceTests;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
 /**
  * RCP model tests.
  *
  * @author scheglov_ke
  */
-public class ModelTests extends DesignerSuiteTests {
-  public static Test suite() {
-    TestSuite suite = new TestSuite("org.eclipse.wb.rcp.model");
-    suite.addTest(WidgetTests.suite());
-    suite.addTest(UtilTests.suite());
-    suite.addTest(PropertyTests.suite());
-    suite.addTest(LayoutTests.suite());
-    suite.addTest(JFaceTests.suite());
-    suite.addTest(FormsTests.suite());
-    suite.addTest(TheRcpTests.suite());
-    suite.addTest(E4Tests.suite());
-    return suite;
-  }
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	WidgetTests.class,
+	UtilTests.class,
+    ResourceTests.class,
+    LayoutTests.class,
+    JFaceTests.class,
+    FormsTests.class, 
+    TheRcpTests.class, 
+    E4Tests.class,
+
+})
+public class ModelTests {
 }
