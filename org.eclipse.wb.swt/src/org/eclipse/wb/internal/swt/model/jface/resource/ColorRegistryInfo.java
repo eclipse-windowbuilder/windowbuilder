33404787c063bb08a9c40e47d6aca93f83c27183
Patrick Ziegler <ziegler.patrick@hotmail.de>
2023-09-29 18:18:14 +0200

    Fix: Potential memory leak due to undisposed resource registries
    When we create instances of various ResourceRegistrys, for example
    ColorRegistry, they usually want to be disposed with Display, so they
    use Display#disposeExec(Runnable). But our Display instance lives all
    time while Eclipse lives. So, practically we have memory leak: we keep
    in memory instance of ResourceRegistry. its allocated resources, and
    what is much worse - instance of ClassLoader that loaded this
    ReourceRegistry.
    
    The old implementation that used to explicitly dispose those objects was
    no longer functional and has been repaired.

--- org.eclipse.wb.swt/src/org/eclipse/wb/internal/swt/model/jface/resource/ColorRegistryInfo.java
+++ org.eclipse.wb.swt/src/org/eclipse/wb/internal/swt/model/jface/resource/ColorRegistryInfo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Google, Inc.
+ * Copyright (c) 2011, 2023 Google, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,6 +13,9 @@ package org.eclipse.wb.internal.swt.model.jface.resource;
 import org.eclipse.wb.internal.core.model.creation.CreationSupport;
 import org.eclipse.wb.internal.core.model.description.ComponentDescription;
 import org.eclipse.wb.internal.core.utils.ast.AstEditor;
+import org.eclipse.wb.internal.core.utils.reflect.ReflectionUtils;
+
+import org.eclipse.jface.resource.ColorRegistry;
 
 /**
  * Implementation model for {@link org.eclipse.jface.resource.ColorRegistry}.
@@ -30,5 +33,13 @@ public final class ColorRegistryInfo extends ResourceRegistryInfo {
 			ComponentDescription description,
 			CreationSupport creationSupport) throws Exception {
 		super(editor, description, creationSupport);
+	}
+
+	@Override
+	public Runnable getDisposeRunnable() {
+		if (getObject() instanceof ColorRegistry registry) {
+			return (Runnable) ReflectionUtils.getFieldObject(registry, "displayRunnable");
+		}
+		return null;
 	}
 }
