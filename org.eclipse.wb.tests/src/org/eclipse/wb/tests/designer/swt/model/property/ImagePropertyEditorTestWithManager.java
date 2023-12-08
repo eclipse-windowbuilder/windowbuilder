5353c85180f6ff7a8625e0ac7b66717646a83533
Patrick Ziegler <ziegler.patrick@hotmail.de>
2023-12-08 21:51:13 +0100

    Try to fix random failures in ImagePropertyEditorTestWithManager
    The test_textSource_image_over_classpath2() test sporadically fails. The
    reason seems to be an ClassNotFoundException because the Test class
    hasn't been built by the time we try to access its properties.
    
    Exclipity wait for the auto-build to finish, before proceeding with the
    test should solve this problem.

--- org.eclipse.wb.tests/src/org/eclipse/wb/tests/designer/swt/model/property/ImagePropertyEditorTestWithManager.java
+++ org.eclipse.wb.tests/src/org/eclipse/wb/tests/designer/swt/model/property/ImagePropertyEditorTestWithManager.java
@@ -183,12 +183,12 @@ public class ImagePropertyEditorTestWithManager extends ImagePropertyEditorTest 
 						"  public Test() {",
 						"  }",
 						"}");
+		waitForAutoBuild();
 		// add ResourceManager
 		ManagerContainerInfo.getResourceManagerInfo(shell);
 		// set "image" property
 		shell.addMethodInvocation("setImage(org.eclipse.swt.graphics.Image)", imageSource);
 		shell.refresh();
-		waitEventLoop(10);
 		//
 		Property property = shell.getPropertyByTitle("image");
 		assertEquals(expectedText, PropertyEditorTestUtils.getText(property));
