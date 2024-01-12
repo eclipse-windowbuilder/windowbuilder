40a9245f4431413d4ae2cabfd85404d441459355
Patrick Ziegler <ziegler.patrick@hotmail.de>
2024-01-12 21:58:13 +0100

    Process exposing rules via JAXB
    Note that there is no real distinction between inclusions and
    exclusions, hence why we work directly with the JAXBElement.
    
    See #638

--- org.eclipse.wb.core.java/src/org/eclipse/wb/internal/core/model/description/rules/ExposingRulesRule.java
+++ org.eclipse.wb.core.java/src/org/eclipse/wb/internal/core/model/description/rules/ExposingRulesRule.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Google, Inc.
+ * Copyright (c) 2011, 2024 Google, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,33 +10,36 @@
  *******************************************************************************/
 package org.eclipse.wb.internal.core.model.description.rules;
 
+import org.eclipse.wb.core.databinding.xsd.component.ExposingRuleType;
 import org.eclipse.wb.internal.core.model.description.ComponentDescription;
 import org.eclipse.wb.internal.core.model.description.ExposingMethodRule;
 import org.eclipse.wb.internal.core.model.description.ExposingPackageRule;
 import org.eclipse.wb.internal.core.model.description.ExposingRule;
+import org.eclipse.wb.internal.core.model.description.helpers.ComponentDescriptionHelper.FailableBiConsumer;
 
-import org.apache.commons.digester3.Rule;
-import org.xml.sax.Attributes;
+import jakarta.xml.bind.JAXBElement;
 
 /**
- * The {@link Rule} that adds include/exclude rules for exposed children.
+ * The {@link FailableBiConsumer} that adds include/exclude rules for exposed
+ * children.
  *
  * @author scheglov_ke
  * @coverage core.model.description
  */
-public final class ExposingRulesRule extends AbstractDesignerRule {
+public final class ExposingRulesRule
+		implements FailableBiConsumer<ComponentDescription, JAXBElement<ExposingRuleType>, Exception> {
 	////////////////////////////////////////////////////////////////////////////
 	//
 	// Rule
 	//
 	////////////////////////////////////////////////////////////////////////////
 	@Override
-	public void begin(String namespace, String name, Attributes attributes) throws Exception {
-		ComponentDescription componentDescription = (ComponentDescription) getDigester().peek();
+	public void accept(ComponentDescription componentDescription, JAXBElement<ExposingRuleType> jaxb) throws Exception {
 		// prepare attributes
-		boolean include = "include".equals(name);
-		String packageName = attributes.getValue("package");
-		String methodName = attributes.getValue("method");
+		ExposingRuleType exposingRule = jaxb.getValue();
+		boolean include = "include".equals(jaxb.getName().getLocalPart());
+		String packageName = exposingRule.getPackage();
+		String methodName = exposingRule.getMethod();
 		// add expose rules
 		if (packageName != null) {
 			ExposingRule rule = new ExposingPackageRule(include, packageName);
