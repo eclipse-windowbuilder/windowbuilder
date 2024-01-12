6fd9c4506b643eb07563dcc1cb34a68ff5955d4e
Patrick Ziegler <ziegler.patrick@hotmail.de>
2024-01-12 20:51:51 +0100

    Migrate morphing target resolution to JAXB
    It is important to note that the noInherit property needs to be checked
    before the new morph targets are added, to ensure that any parent
    targets are cleared before the targets of the current class are added.
    
    See #638

--- org.eclipse.wb.core.java/src/org/eclipse/wb/internal/core/model/description/rules/MorphingNoInheritRule.java
+++ org.eclipse.wb.core.java/src/org/eclipse/wb/internal/core/model/description/rules/MorphingNoInheritRule.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Google, Inc.
+ * Copyright (c) 2011, 2024 Google, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,27 +12,24 @@ package org.eclipse.wb.internal.core.model.description.rules;
 
 import org.eclipse.wb.internal.core.model.description.ComponentDescription;
 import org.eclipse.wb.internal.core.model.description.MorphingTargetDescription;
-
-import org.apache.commons.digester3.Rule;
-import org.xml.sax.Attributes;
+import org.eclipse.wb.internal.core.model.description.helpers.ComponentDescriptionHelper.FailableBiConsumer;
 
 /**
- * The {@link Rule} that remove all existed (inherited) {@link MorphingTargetDescription}'s from
- * {@link ComponentDescription}.
+ * The {@link FailableBiConsumer} that remove all existed (inherited)
+ * {@link MorphingTargetDescription}'s from {@link ComponentDescription}.
  *
  * @author sablin_aa
  * @coverage core.model.description
  */
-public final class MorphingNoInheritRule extends Rule {
+public final class MorphingNoInheritRule implements FailableBiConsumer<ComponentDescription, Object, Exception> {
 	////////////////////////////////////////////////////////////////////////////
 	//
 	// Rule
 	//
 	////////////////////////////////////////////////////////////////////////////
 	@Override
-	public void begin(String namespace, String name, Attributes attributes) throws Exception {
+	public void accept(ComponentDescription componentDescription, Object object) throws Exception {
 		// clear morphing targets
-		ComponentDescription componentDescription = (ComponentDescription) getDigester().peek();
 		componentDescription.clearMorphingTargets();
 	}
 }
