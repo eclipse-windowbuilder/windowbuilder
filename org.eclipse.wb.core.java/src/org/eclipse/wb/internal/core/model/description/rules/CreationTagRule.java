9fdbd5810d26d9685ec95dcfcee629f5ae0740ab
Patrick Ziegler <ziegler.patrick@hotmail.de>
2024-01-13 10:42:58 +0100

    Load the component creations via JAXB
    The creation description is expected to be a string. Note that both the
    description and source are optional, hence why the setter invocation has
    to be wrapped via acceptSafe().
    
    See #638

--- org.eclipse.wb.core.java/src/org/eclipse/wb/internal/core/model/description/rules/CreationTagRule.java
+++ org.eclipse.wb.core.java/src/org/eclipse/wb/internal/core/model/description/rules/CreationTagRule.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Google, Inc.
+ * Copyright (c) 2011, 2024 Google, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,28 +10,27 @@
  *******************************************************************************/
 package org.eclipse.wb.internal.core.model.description.rules;
 
+import org.eclipse.wb.core.databinding.xsd.component.TagType;
 import org.eclipse.wb.internal.core.model.description.CreationDescription;
-
-import org.apache.commons.digester3.Rule;
-import org.xml.sax.Attributes;
+import org.eclipse.wb.internal.core.model.description.helpers.ComponentDescriptionHelper.FailableBiConsumer;
 
 /**
- * The {@link Rule} that adds some tag for {@link CreationDescription}.
+ * The {@link FailableBiConsumer} that adds some tag for
+ * {@link CreationDescription}.
  *
  * @author sablin_aa
  * @coverage core.model.description
  */
-public final class CreationTagRule extends AbstractDesignerRule {
+public final class CreationTagRule implements FailableBiConsumer<CreationDescription, TagType, Exception> {
 	////////////////////////////////////////////////////////////////////////////
 	//
 	// Rule
 	//
 	////////////////////////////////////////////////////////////////////////////
 	@Override
-	public void begin(String namespace, String name, Attributes attributes) throws Exception {
-		String tag = getRequiredAttribute(name, attributes, "name");
-		String value = getRequiredAttribute(name, attributes, "value");
-		CreationDescription creationDescription = (CreationDescription) getDigester().peek();
+	public void accept(CreationDescription creationDescription, TagType tagType) throws Exception {
+		String tag = tagType.getName();
+		String value = tagType.getValue();
 		creationDescription.putTag(tag, value);
 	}
 }
