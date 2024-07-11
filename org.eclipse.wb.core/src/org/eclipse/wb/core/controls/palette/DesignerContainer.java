7138edaf60406c0a0c01be51114b0506bfd6c808
Patrick Ziegler <ziegler.patrick@hotmail.de>
2024-07-11 23:04:53 +0200

    [GEF] Make IContainer compatible with PaletteDrawer
    Because the IContainer interface is API, we can't simply rename the
    methods to match the ones that are used by GEF. Instead, a new
    DesignerContainer class is created, which serves as an adapter between
    the IContainer interface and the PaletteDrawer class.
    
    The IContainer interface has been marked as deprecated and "for
    removal", meaning that they can be removed after a two-year grace
    period, without breaking API.

---
+++ org.eclipse.wb.core/src/org/eclipse/wb/core/controls/palette/DesignerContainer.java
@@ -0,0 +1,81 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Patrick Ziegler and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Patrick Ziegler - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.wb.core.controls.palette;
+
+import org.eclipse.gef.palette.PaletteDrawer;
+
+import java.util.List;
+
+/**
+ * Category - collection of {@link DesignerEntry}.
+ */
+@SuppressWarnings("removal")
+public abstract class DesignerContainer extends PaletteDrawer implements ICategory {
+
+	protected DesignerContainer(String label, String desc) {
+		super(label, null);
+		setDescription(desc);
+	}
+
+	/**
+	 * @noreference This method is not intended to be referenced by clients.
+	 * @nooverride This method is not intended to be re-implemented or extended by
+	 *             clients.
+	 * @deprecated Use {@link #getLabel()} instead.
+	 */
+	@Override
+	@Deprecated(since = "1.17.0", forRemoval = true)
+	public final String getText() {
+		return getLabel();
+	}
+
+	/**
+	 * @noreference This method is not intended to be referenced by clients.
+	 * @nooverride This method is not intended to be re-implemented or extended by
+	 *             clients.
+	 * @deprecated Use {@link #getDescription()} instead.
+	 */
+	@Override
+	@Deprecated(since = "1.17.0", forRemoval = true)
+	public final String getToolTipText() {
+		return getDescription();
+	}
+
+	/**
+	 * @return <code>true</code> if this category is open.
+	 * @noreference This method is not intended to be referenced by clients.
+	 * @nooverride This method is not intended to be re-implemented or extended by
+	 *             clients.
+	 * @deprecated Set in the {@code DrawerFigure}.
+	 */
+	@Override
+	@Deprecated(since = "1.17.0", forRemoval = true)
+	public abstract boolean isOpen();
+
+	/**
+	 * Sets if this category is open.
+	 *
+	 * @noreference This method is not intended to be referenced by clients.
+	 * @nooverride This method is not intended to be re-implemented or extended by
+	 *             clients.
+	 * @deprecated Set in the {@code DrawerFigure}.
+	 */
+	@Override
+	@Deprecated(since = "1.17.0", forRemoval = true)
+	public abstract void setOpen(boolean b);
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public List<DesignerEntry> getEntries() {
+		return (List<DesignerEntry>) super.getChildren();
+	}
+
+}
