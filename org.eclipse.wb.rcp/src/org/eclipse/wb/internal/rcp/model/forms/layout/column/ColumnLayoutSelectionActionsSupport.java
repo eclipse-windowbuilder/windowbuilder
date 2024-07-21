0f89c110d82e9b5bea7368b71341ef97177c1762
Patrick Ziegler <ziegler.patrick@hotmail.de>
2024-07-21 19:46:24 +0200

    Replace ColumnLayout icons with new high-DPI alignment variants
    This removes the ColumnLayout GIF files in org.eclipse.wb.rcp and
    updates all references to use the icons provided by CoreImages.

--- org.eclipse.wb.rcp/src/org/eclipse/wb/internal/rcp/model/forms/layout/column/ColumnLayoutSelectionActionsSupport.java
+++ org.eclipse.wb.rcp/src/org/eclipse/wb/internal/rcp/model/forms/layout/column/ColumnLayoutSelectionActionsSupport.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Google, Inc.
+ * Copyright (c) 2011, 2024 Google, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,14 +10,15 @@
  *******************************************************************************/
 package org.eclipse.wb.internal.rcp.model.forms.layout.column;
 
+import org.eclipse.wb.core.editor.constants.CoreImages;
 import org.eclipse.wb.core.model.ObjectInfo;
 import org.eclipse.wb.core.model.broadcast.ObjectEventListener;
 import org.eclipse.wb.internal.core.model.util.ObjectInfoAction;
-import org.eclipse.wb.internal.rcp.Activator;
 import org.eclipse.wb.internal.rcp.model.ModelMessages;
 import org.eclipse.wb.internal.swt.model.widgets.IControlInfo;
 
 import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.forms.widgets.ColumnLayoutData;
 
 import java.util.ArrayList;
@@ -71,32 +72,32 @@ ObjectEventListener {
 		addAlignmentAction(
 				actions,
 				dataInfos,
-				"left.gif",
+				CoreImages.ALIGNMENT_H_MENU_LEFT,
 				ModelMessages.ColumnLayoutSelectionActionsSupport_alignmentLeft,
 				ColumnLayoutData.LEFT);
 		addAlignmentAction(
 				actions,
 				dataInfos,
-				"center.gif",
+				CoreImages.ALIGNMENT_H_MENU_CENTER,
 				ModelMessages.ColumnLayoutSelectionActionsSupport_alignmentCenter,
 				ColumnLayoutData.CENTER);
 		addAlignmentAction(
 				actions,
 				dataInfos,
-				"right.gif",
+				CoreImages.ALIGNMENT_H_MENU_RIGHT,
 				ModelMessages.ColumnLayoutSelectionActionsSupport_alignmentRight,
 				ColumnLayoutData.RIGHT);
 		addAlignmentAction(
 				actions,
 				dataInfos,
-				"fill.gif",
+				CoreImages.ALIGNMENT_H_MENU_FILL,
 				ModelMessages.ColumnLayoutSelectionActionsSupport_alignmentFill,
 				ColumnLayoutData.FILL);
 	}
 
 	private void addAlignmentAction(List<Object> actions,
 			List<IColumnLayoutDataInfo> dataInfos,
-			String iconPath,
+			ImageDescriptor icon,
 			String tooltip,
 			int alignment) throws Exception {
 		boolean isChecked = true;
@@ -109,7 +110,7 @@ ObjectEventListener {
 		}
 		// create action
 		AlignmentAction action =
-				new AlignmentAction(dataInfos, iconPath, tooltip, isChecked, alignment);
+				new AlignmentAction(dataInfos, icon, tooltip, isChecked, alignment);
 		actions.add(action);
 	}
 
@@ -128,13 +129,12 @@ ObjectEventListener {
 		////////////////////////////////////////////////////////////////////////////
 		public AbstractAction(int style,
 				List<IColumnLayoutDataInfo> dataInfos,
-				String iconPath,
+				ImageDescriptor icon,
 				String tooltip,
 				boolean checked) {
 			super(m_layout.getUnderlyingModel(), "", style);
 			m_dataInfos = dataInfos;
-			String path = "info/layout/ColumnLayout/" + iconPath;
-			setImageDescriptor(Activator.getImageDescriptor(path));
+			setImageDescriptor(icon);
 			setToolTipText(tooltip);
 			setChecked(checked);
 		}
@@ -167,11 +167,11 @@ ObjectEventListener {
 		//
 		////////////////////////////////////////////////////////////////////////////
 		public AlignmentAction(List<IColumnLayoutDataInfo> dataInfos,
-				String iconPath,
+				ImageDescriptor icon,
 				String tooltip,
 				boolean checked,
 				int alignment) {
-			super(AS_RADIO_BUTTON, dataInfos, iconPath, tooltip, checked);
+			super(AS_RADIO_BUTTON, dataInfos, icon, tooltip, checked);
 			m_alignment = alignment;
 		}
 
