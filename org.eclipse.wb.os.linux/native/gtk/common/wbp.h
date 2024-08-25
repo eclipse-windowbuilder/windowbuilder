922d6dcc30bb7a7f1f541da5510fc8ed375c3c2a
Patrick Ziegler <ziegler.patrick@hotmail.de>
2024-08-25 12:43:16 +0200

    [GTK] Use primitive types in native method invocation.
    The native bindings where created with both 32 and 64 bit in mind. As a
    result, the native methods are invoked using Integer and Long objects,
    rather than primitive types, causing a lot of boxing and unboxing within
    the C layer. With this change, we exclusively use the long type to
    handle memory addresses, making 64bit the "default". Any architectures
    with shorter memory addresses have to cast their values to e.g. an int.
    
    Support for older architecturs (if ever needed) is done on the C layer
    via an explicit type cast (utilizing the pre-processor) when calling the
    proper GTK methods. While this introduces a small overhead for
    non-64-bit architectures it is very likely compensated by no longer
    having to work with the Java objects.

--- org.eclipse.wb.os.linux/native/gtk/common/wbp.h
+++ org.eclipse.wb.os.linux/native/gtk/common/wbp.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Google, Inc.
+ * Copyright (c) 2011, 2024 Google, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,14 +14,24 @@
 #include <gtk/gtk.h>
 #include <gdk/gdkx.h>
 
-#include "utils.h"
+#if (defined(__LP64__) && (!defined(WBP_ARCH64))) || defined(_WIN64)
+	#if !defined(WBP_ARCH64)
+		#define WBP_ARCH64
+	#endif
+#endif /*__LP64__*/
+
+#define JHANDLE jlong
+#ifdef WBP_ARCH64
+	#define CHANDLE jlong
+#else
+	#define CHANDLE jint
+#endif
 
 #define GTK_WIDGET_X(arg0) (arg0)->allocation.x
 #define GTK_WIDGET_Y(arg0) (arg0)->allocation.y
 #define GTK_WIDGET_WIDTH(arg0) (arg0)->allocation.width
 #define GTK_WIDGET_HEIGHT(arg0) (arg0)->allocation.height
 
-#define JHANDLE jobject
 #define CALLBACK_SIG "(Ljava/lang/Number;Ljava/lang/Number;)V"
 #define OS_NATIVE(func) Java_org_eclipse_wb_internal_os_linux_OSSupportLinux_##func
 
