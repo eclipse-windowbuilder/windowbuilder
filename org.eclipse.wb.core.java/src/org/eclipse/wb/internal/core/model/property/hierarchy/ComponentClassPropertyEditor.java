e283277f10d0ff10c53f93aba8c2f609a5ffcaab
Patrick Ziegler <ziegler.patrick@hotmail.de>
2025-03-14 06:15:30 +0100

    Remove unused width parameter when creating the tooltip control

--- org.eclipse.wb.core.java/src/org/eclipse/wb/internal/core/model/property/hierarchy/ComponentClassPropertyEditor.java
+++ org.eclipse.wb.core.java/src/org/eclipse/wb/internal/core/model/property/hierarchy/ComponentClassPropertyEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2023 Google, Inc.
+ * Copyright (c) 2011, 2025 Google, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -86,7 +86,6 @@ public final class ComponentClassPropertyEditor extends TextDisplayPropertyEdito
 			@Override
 			public Control createTooltipControl(Property property,
 					Composite parent,
-					int availableWidth,
 					final IPropertyTooltipSite site) {
 				// prepare hierarchy
 				List<Class<?>> classes = new ArrayList<>();
