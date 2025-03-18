b3f382b10af8c472af176069a572df84a8548364
Patrick Ziegler <ziegler.patrick@hotmail.de>
2025-03-18 20:29:52 +0100

    Add converter for AWT Cursor, Font and ComponentOrientation
    This allows those types to be used within a Customizer, without throwing
    an exception.

---
+++ org.eclipse.wb.swing/src/org/eclipse/wb/internal/swing/model/property/converter/ComponentOrientationConverter.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Patrick Ziegler and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Patrick Ziegler - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.wb.internal.swing.model.property.converter;
+
+import org.eclipse.wb.core.model.JavaInfo;
+import org.eclipse.wb.internal.core.model.property.converter.ExpressionConverter;
+
+import java.awt.ComponentOrientation;
+
+public class ComponentOrientationConverter extends ExpressionConverter {
+	@Override
+	public String toJavaSource(JavaInfo javaInfo, Object value) throws Exception {
+		if (value == null) {
+			return "(java.awt.ComponentOrientation) null";
+		}
+
+		ComponentOrientation orientation = (ComponentOrientation) value;
+		if (orientation.isLeftToRight()) {
+			return "java.awt.ComponentOrientation.LEFT_TO_RIGHT";
+		}
+		return "java.awt.ComponentOrientation.RIGHT_TO_LEFT";
+	}
+}
---
+++ org.eclipse.wb.swing/src/org/eclipse/wb/internal/swing/model/property/converter/CursorConverter.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Patrick Ziegler and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Patrick Ziegler - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.wb.internal.swing.model.property.converter;
+
+import org.eclipse.wb.core.model.JavaInfo;
+import org.eclipse.wb.internal.core.model.property.converter.ExpressionConverter;
+import org.eclipse.wb.internal.core.model.property.converter.IntegerConverter;
+import org.eclipse.wb.internal.core.model.property.converter.StringConverter;
+
+import java.awt.Cursor;
+
+public class CursorConverter extends ExpressionConverter {
+	@Override
+	public String toJavaSource(JavaInfo javaInfo, Object value) throws Exception {
+		if (value == null) {
+			return "(java.awt.Cursor) null";
+		}
+
+		Cursor cursor = (Cursor) value;
+		if (cursor.getType() == Cursor.CUSTOM_CURSOR) {
+			return "java.awt.Cursor.getSystemCustomCursor(%s)"
+					.formatted(StringConverter.INSTANCE.toJavaSource(javaInfo, cursor.getName()));
+		}
+
+		for (CursorInfo cursorInfo : CursorInfo.getCursors()) {
+			if (cursorInfo.type() == cursor.getType()) {
+				return "java.awt.Cursor.getPredefinedCursor(java.awt.Cursor.%s)"
+						.formatted(cursorInfo.name());
+			}
+		}
+		return "java.awt.Cursor.getPredefinedCursor(%s)"
+				.formatted(IntegerConverter.INSTANCE.toJavaSource(javaInfo, cursor.getType()));
+	}
+
+}
