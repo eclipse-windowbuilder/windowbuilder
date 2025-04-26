443350625600122ed8c84fae6cb340091ccce206
Patrick Ziegler <ziegler.patrick@hotmail.de>
2025-04-26 14:26:55 +0200

    Don't call setAccessible() on the component constructors.
    This avoids the IllegalAccessException thrown when processing the
    package-private BoxLayout constructor and is also redundant, as the
    necessary changes are also done by the ReflectionUtils class.
    
    Contributes to
    https://github.com/eclipse-windowbuilder/windowbuilder/issues/1027

--- org.eclipse.wb.core.java/src/org/eclipse/wb/internal/core/model/description/helpers/ComponentDescriptionHelper.java
+++ org.eclipse.wb.core.java/src/org/eclipse/wb/internal/core/model/description/helpers/ComponentDescriptionHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2024 Google, Inc. and others.
+ * Copyright (c) 2011, 2025 Google, Inc. and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -534,7 +534,6 @@ public final class ComponentDescriptionHelper {
 			throws Exception {
 		Class<?> componentClass = componentDescription.getComponentClass();
 		for (Constructor<?> constructor : componentClass.getDeclaredConstructors()) {
-			constructor.setAccessible(true);
 			ConstructorDescription constructorDescription = new ConstructorDescription(componentClass);
 			// add parameter descriptions of constructor
 			for (Class<?> parameterType : constructor.getParameterTypes()) {
