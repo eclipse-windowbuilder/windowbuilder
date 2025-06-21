97c7f78c605c4d5b286b01bb83533c0cf717d86d
Patrick Ziegler <ziegler.patrick@hotmail.de>
2025-06-21 13:56:44 +0200

    Remove AMS component tests
    This removes the test cases for the VarmenuConstraints and
    VarmenuLayout.
    
    At some point, support for those classes was provided via the
    org.eclipse.wb.swing.asm plugin. However, this plugin was removed when
    WindowBuilder was donated to the Eclipse foundation. Those tests
    therefore cover functionality that is not provided by WindowBuilder

--- org.eclipse.wb.tests/src/org/eclipse/wb/tests/designer/swing/SwingTests.java
+++ org.eclipse.wb.tests/src/org/eclipse/wb/tests/designer/swing/SwingTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2024 Google, Inc. and others.
+ * Copyright (c) 2011, 2025 Google, Inc. and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -12,7 +12,6 @@
  *******************************************************************************/
 package org.eclipse.wb.tests.designer.swing;
 
-import org.eclipse.wb.tests.designer.swing.ams.AmsTests;
 import org.eclipse.wb.tests.designer.swing.laf.LookAndFeelTest;
 import org.eclipse.wb.tests.designer.swing.model.ModelTests;
 import org.eclipse.wb.tests.designer.swing.swingx.SwingXTests;
@@ -29,7 +28,6 @@ import org.junit.platform.suite.api.Suite;
 	ConvertersTest.class,
 	CustomizeTest.class,
 	ModelTests.class,
-	AmsTests.class,
 	SwingXTests.class,
 	//  WaitForMemoryProfilerTest.class,
 })
