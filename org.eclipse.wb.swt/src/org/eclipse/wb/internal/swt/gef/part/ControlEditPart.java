210df50ef91a31d610ac33eaaaf4a9bcdd4c43d4
Patrick Ziegler <ziegler.patrick@hotmail.de>
2025-06-24 18:57:10 +0200

    [GEF] Remove explicit type cast in getTargetEditPart()

--- org.eclipse.wb.swt/src/org/eclipse/wb/internal/swt/gef/part/ControlEditPart.java
+++ org.eclipse.wb.swt/src/org/eclipse/wb/internal/swt/gef/part/ControlEditPart.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2024 Google, Inc. and others.
+ * Copyright (c) 2011, 2025 Google, Inc. and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -14,9 +14,9 @@ package org.eclipse.wb.internal.swt.gef.part;
 
 import org.eclipse.wb.core.gef.part.AbstractComponentEditPart;
 import org.eclipse.wb.core.gef.policy.TabOrderContainerEditPolicy;
-import org.eclipse.wb.gef.core.EditPart;
 import org.eclipse.wb.internal.swt.model.widgets.ControlInfo;
 
+import org.eclipse.gef.EditPart;
 import org.eclipse.gef.Request;
 
 /**
