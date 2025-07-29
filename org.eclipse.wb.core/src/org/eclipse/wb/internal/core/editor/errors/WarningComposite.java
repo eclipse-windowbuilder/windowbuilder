41e7f732d520096218332c49644e4893f00f5343
Patrick Ziegler <ziegler.patrick@hotmail.de>
2025-07-29 19:05:49 +0200

    Disable "Basic UI" mode by default
    With https://github.com/eclipse-windowbuilder/windowbuilder/pull/156, a
    new mode was added that supresses e.g. the buttons generated in the
    error dialog. This mode is activated via a preference, but can't be done
    within the UI. Meaning that those buttons are never visible to the user
    and thus make error reporting more difficult.
    
    Given that this was added as a new functionality, the mode needs to be
    opt-in, rather than opt-out.

--- org.eclipse.wb.core/src/org/eclipse/wb/internal/core/editor/errors/WarningComposite.java
+++ org.eclipse.wb.core/src/org/eclipse/wb/internal/core/editor/errors/WarningComposite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2023 Google, Inc.
+ * Copyright (c) 2011, 2025 Google, Inc. and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -57,7 +57,7 @@ public abstract class WarningComposite extends Composite {
 		wbBasic = InstanceScope.INSTANCE.getNode(
 				IEditorPreferenceConstants.WB_BASIC_UI_PREFERENCE_NODE).getBoolean(
 						IEditorPreferenceConstants.WB_BASIC_UI,
-						true);
+						false);
 		GridLayoutFactory.create(this);
 		{
 			Composite titleComposite = new Composite(this, SWT.NONE);
