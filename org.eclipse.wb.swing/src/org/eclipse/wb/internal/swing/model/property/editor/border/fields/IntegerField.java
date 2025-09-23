5c70761fa02344a17bd04193444326b76754052e
Patrick Ziegler <ziegler.patrick@hotmail.de>
2025-09-23 06:54:44 +0200

    Remove checked exception from Border accessors
    The setValue() of the BooleanField, ColorField, IntegerField and
    TextField don't throw a checked exception, so there is no purpose for
    the signature to contain "throws Exception".

--- org.eclipse.wb.swing/src/org/eclipse/wb/internal/swing/model/property/editor/border/fields/IntegerField.java
+++ org.eclipse.wb.swing/src/org/eclipse/wb/internal/swing/model/property/editor/border/fields/IntegerField.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Google, Inc.
+ * Copyright (c) 2011, 2025 Google, Inc. and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -59,7 +59,7 @@ public final class IntegerField extends AbstractBorderField {
 	/**
 	 * Sets the value, that should correspond to the one of the field values.
 	 */
-	public void setValue(int value) throws Exception {
+	public void setValue(int value) {
 		m_spinner.setSelection(value);
 	}
 
--- org.eclipse.wb.swing/src/org/eclipse/wb/internal/swing/model/property/editor/border/fields/TextField.java
+++ org.eclipse.wb.swing/src/org/eclipse/wb/internal/swing/model/property/editor/border/fields/TextField.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Google, Inc.
+ * Copyright (c) 2011, 2025 Google, Inc. and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -59,7 +59,7 @@ public final class TextField extends AbstractBorderField {
 	/**
 	 * Sets the value to edit.
 	 */
-	public void setValue(String value) throws Exception {
+	public void setValue(String value) {
 		m_text.removeListener(SWT.Modify, m_modifyListener);
 		try {
 			m_text.setText(value);
