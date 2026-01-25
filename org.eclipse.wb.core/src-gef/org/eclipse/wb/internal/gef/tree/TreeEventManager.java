febf716e60a06e877decba3c6a320ba7c11a3e9b
Patrick Ziegler <ziegler.patrick@hotmail.de>
2026-01-25 11:04:04 +0100

    Mark OSSupport.isPlusMinusTreeClick() for removal #1302
    This method is only implemented on Windows and even there, it makes no
    difference whether this method is called or not. With this change, this
    method is marked for removal and all its usages removed.
    
    Closes https://github.com/eclipse-windowbuilder/windowbuilder/issues/1302

--- org.eclipse.wb.core/src-gef/org/eclipse/wb/internal/gef/tree/TreeEventManager.java
+++ org.eclipse.wb.core/src-gef/org/eclipse/wb/internal/gef/tree/TreeEventManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2025 Google, Inc. and others.
+ * Copyright (c) 2011, 2026 Google, Inc. and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -16,7 +16,6 @@ import org.eclipse.wb.gef.core.IEditPartViewer;
 import org.eclipse.wb.internal.gef.core.EditDomain;
 import org.eclipse.wb.internal.gef.tree.dnd.TreeDropListener;
 import org.eclipse.wb.internal.gef.tree.dnd.TreeTransfer;
-import org.eclipse.wb.os.OSSupport;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.DND;
@@ -29,7 +28,6 @@ import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.events.MouseMoveListener;
 import org.eclipse.swt.events.MouseTrackListener;
 import org.eclipse.swt.widgets.Tree;
-import org.eclipse.swt.widgets.TreeItem;
 
 /**
  * @author lobas_av
@@ -106,9 +104,6 @@ MouseTrackListener {
 
 	@Override
 	public void mouseDown(MouseEvent event) {
-		if (isPlusMinusClick(m_tree, event.x, event.y)) {
-			return;
-		}
 		// OK, send to domain
 		if (m_domain != null) {
 			m_domain.mouseDown(event, m_viewer);
@@ -159,17 +154,5 @@ MouseTrackListener {
 
 	@Override
 	public void mouseHover(MouseEvent event) {
-	}
-
-	////////////////////////////////////////////////////////////////////////////
-	//
-	// Utils
-	//
-	////////////////////////////////////////////////////////////////////////////
-	/**
-	 * @return <code>true</code> if pointer is over {@link TreeItem} plus/minus sign.
-	 */
-	private static boolean isPlusMinusClick(Tree tree, int x, int y) {
-		return OSSupport.get().isPlusMinusTreeClick(tree, x, y);
 	}
 }
