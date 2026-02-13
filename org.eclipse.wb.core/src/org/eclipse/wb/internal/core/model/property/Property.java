aea92a4dd193676ec0da592a592d8185b4a688df
Patrick Ziegler <ziegler.patrick@hotmail.de>
2026-02-13 21:15:36 +0100

    Add `toString()` implementations for Property and PropertyInfo

--- org.eclipse.wb.core/src/org/eclipse/wb/internal/core/model/property/Property.java
+++ org.eclipse.wb.core/src/org/eclipse/wb/internal/core/model/property/Property.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2024 Google, Inc. and others.
+ * Copyright (c) 2011, 2026 Google, Inc. and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -179,5 +179,10 @@ public abstract class Property implements IAdaptable {
 		if (m_arbitraryMap != null) {
 			m_arbitraryMap.remove(key);
 		}
+	}
+
+	@Override
+	public String toString() {
+		return "[Property] %s".formatted(getTitle());
 	}
 }
