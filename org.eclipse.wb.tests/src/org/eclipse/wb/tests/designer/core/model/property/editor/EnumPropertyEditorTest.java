ecfe2c42cab678c0fa6e4c4fcc9fd6523c5a53a5
Patrick Ziegler <ziegler.patrick@hotmail.de>
2026-02-13 19:52:15 +0100

    Fix potential NPE in EnumPropertyEditor
    When an unknown property is selected, a `null` text is returned. Because
    the SWT combo doesn't support `null` strings, an exception might be
    thrown. Instead, an empty string has to be returned.
    
    Follow-up on 6ee0cd89442090296846298add13aa9585cd5a08

--- org.eclipse.wb.tests/src/org/eclipse/wb/tests/designer/core/model/property/editor/EnumPropertyEditorTest.java
+++ org.eclipse.wb.tests/src/org/eclipse/wb/tests/designer/core/model/property/editor/EnumPropertyEditorTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2024 Google, Inc. and others.
+ * Copyright (c) 2011, 2026 Google, Inc. and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -96,7 +96,7 @@ public class EnumPropertyEditorTest extends SwingModelTest {
 		GenericProperty property = (GenericProperty) panel.getPropertyByTitle("foo");
 		EnumPropertyEditor editor = (EnumPropertyEditor) property.getEditor();
 		// text
-		assertNull(editor.getText(property));
+		assertEquals("", editor.getText(property));
 		assertNull(editor.getClipboardSource(property));
 	}
 
