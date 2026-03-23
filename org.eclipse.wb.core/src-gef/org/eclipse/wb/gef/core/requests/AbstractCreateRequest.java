dc7db65da68f5f517c2e80b5dd9b146d44355031
Patrick Ziegler <ziegler.patrick@hotmail.de>
2026-03-23 21:52:03 +0100

    [GEF] AbstractCreateRequest should keep track of relative location
    The relative mouse location is required when translating coordinates
    from one viewer to another. For example from the ruler to the design
    viewer when displaying layout hints. Relative coordinates are also what
    the GEF requests expect.

--- org.eclipse.wb.core/src-gef/org/eclipse/wb/gef/core/requests/AbstractCreateRequest.java
+++ org.eclipse.wb.core/src-gef/org/eclipse/wb/gef/core/requests/AbstractCreateRequest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2023 Google, Inc.
+ * Copyright (c) 2011, 2026 Google, Inc. and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -13,6 +13,7 @@
 package org.eclipse.wb.gef.core.requests;
 
 import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.gef.Request;
 import org.eclipse.gef.requests.DropRequest;
 import org.eclipse.gef.requests.LocationRequest;
@@ -23,6 +24,7 @@ import org.eclipse.gef.requests.LocationRequest;
  */
 public abstract class AbstractCreateRequest extends LocationRequest implements DropRequest {
 	private Dimension m_size;
+	private Point m_location;
 
 	////////////////////////////////////////////////////////////////////////////
 	//
@@ -34,6 +36,40 @@ public abstract class AbstractCreateRequest extends LocationRequest implements D
 
 	public AbstractCreateRequest(Object type) {
 		super(type);
+	}
+
+	/**
+	 * @deprecated Use {@link #getRelativeLocation()} instead.
+	 */
+	@Override
+	@Deprecated
+	public Point getLocation() {
+		return super.getLocation();
+	}
+
+	/**
+	 * @deprecated Use {@link #setRelativeLocation(Point)} instead.
+	 */
+	@Override
+	@Deprecated
+	public void setLocation(Point p) {
+		super.setLocation(p);
+	}
+
+	/**
+	 * @return The current mouse location.
+	 */
+	public Point getRelativeLocation() {
+		return m_location;
+	}
+
+	/**
+	 * Sets the current location.
+	 *
+	 * @param p The current location.
+	 */
+	public void setRelativeLocation(Point location) {
+		m_location = location;
 	}
 
 	////////////////////////////////////////////////////////////////////////////
@@ -66,6 +102,7 @@ public abstract class AbstractCreateRequest extends LocationRequest implements D
 	public void copyStateFrom(Request _source) {
 		if (_source instanceof AbstractCreateRequest source) {
 			setLocation(source.getLocation());
+			setRelativeLocation(source.getRelativeLocation());
 			setSize(source.getSize());
 		}
 	}
