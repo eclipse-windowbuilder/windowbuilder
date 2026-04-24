def68fed8bc8e256923dc579d17cdadd685fa2af
Patrick Ziegler <ziegler.patrick@hotmail.de>
2026-04-24 21:56:14 +0200

    Reduce code duplication in LayoutNameSupport
    The `templateLayoutName` preference is the same between Swing and SWT,
    so the preference constants can be moved to the Core plugin. Due to
    split packages, this then means that the `IPreferenceConstants` in the
    Java plugin has to be removed, as it conflicts with the class in the
    Core plugin. But which doesn't matter, as they are otherwise identical.
    
    This then means that the `getTemplate()` can be moved up to the parent
    class. Only the list of available templates needs to be known, which is
    declared via the new, abstract `getTemplates()` method.
    
    Additionally, the `getValueMap()` method can be partially moved to the
    base class, because the `layoutAcronym` and `layoutClassName` styles are
    shared. The method is overridden to add Swing and SWT-specific layouts.

--- org.eclipse.wb.core.java/src/org/eclipse/wb/internal/core/model/layout/LayoutNameSupport.java
+++ org.eclipse.wb.core.java/src/org/eclipse/wb/internal/core/model/layout/LayoutNameSupport.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Google, Inc.
+ * Copyright (c) 2011, 2026 Google, Inc. and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -14,6 +14,12 @@ package org.eclipse.wb.internal.core.model.layout;
 
 import org.eclipse.wb.core.model.JavaInfo;
 import org.eclipse.wb.internal.core.model.variable.SyncParentChildVariableNameSupport;
+import org.eclipse.wb.internal.core.preferences.IPreferenceConstants;
+
+import org.eclipse.jface.preference.IPreferenceStore;
+
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Support for managing name of <code>Layout</code>, so that it corresponds to the name of its
@@ -22,9 +28,7 @@ import org.eclipse.wb.internal.core.model.variable.SyncParentChildVariableNameSu
  * @author sablin_aa
  * @coverage core.model.layout
  */
-public abstract class LayoutNameSupport<T extends JavaInfo>
-extends
-SyncParentChildVariableNameSupport<T> {
+public abstract class LayoutNameSupport<T extends JavaInfo> extends SyncParentChildVariableNameSupport<T> {
 	////////////////////////////////////////////////////////////////////////////
 	//
 	// Constructor
@@ -32,5 +36,31 @@ SyncParentChildVariableNameSupport<T> {
 	////////////////////////////////////////////////////////////////////////////
 	public LayoutNameSupport(T layout) {
 		super(layout);
+	}
+
+	/**
+	 * @return All supported layout patterns.
+	 */
+	protected abstract String[] getTemplates();
+
+	@Override
+	protected Map<String, String> getValueMap() {
+		// prepare variables
+		Map<String, String> valueMap = new TreeMap<>();
+		{
+			valueMap.put("layoutAcronym", getAcronym());
+			valueMap.put("layoutClassName", getClassName());
+		}
+		return valueMap;
+	}
+
+	@Override
+	protected String getTemplate() {
+		IPreferenceStore preferences = m_childInfo.getDescription().getToolkit().getPreferences();
+		String template = preferences.getString(IPreferenceConstants.P_LAYOUT_NAME_TEMPLATE);
+		if (!isValidTemplate(getTemplates(), template)) {
+			template = getTemplateForDefault();
+		}
+		return template;
 	}
 }
--- org.eclipse.wb.swing/src/org/eclipse/wb/internal/swing/preferences/IPreferenceConstants.java
+++ org.eclipse.wb.swing/src/org/eclipse/wb/internal/swing/preferences/IPreferenceConstants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Google, Inc.
+ * Copyright (c) 2011, 2026 Google, Inc. and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -18,12 +18,6 @@ package org.eclipse.wb.internal.swing.preferences;
  * @author scheglov_ke
  * @coverage swing.preferences
  */
-public interface IPreferenceConstants
-extends
-org.eclipse.wb.internal.core.preferences.IPreferenceConstants {
+public interface IPreferenceConstants extends org.eclipse.wb.internal.core.preferences.IPreferenceConstants {
 	String TOOLKIT_ID = "org.eclipse.wb.swing";
-	/**
-	 * The template for <code>Layout</code> variable name.
-	 */
-	String P_LAYOUT_NAME_TEMPLATE = "templateLayoutName";
 }
