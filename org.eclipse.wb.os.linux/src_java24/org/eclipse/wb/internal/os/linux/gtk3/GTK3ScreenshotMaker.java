25a808cf45dc2400f282eff356348cfc529bab16
Patrick Ziegler <ziegler.patrick@hotmail.de>
2026-06-18 19:42:47 +0200

    Rewrite GTK3 screenshot logic to work with Wayland #633
    For the longest time, screenshots on Linux were done by capturing parts
    of the window where a given widget was located. This approach is
    inherently incompatible with constraints imposed by Wayland, resulting
    in a blank preview.
    
    Instead of working on the window, we directly work on the widget
    (similar to the SWT implementation). This approach requires the widget
    to be fully rendered.
    
    Note that this new implementation is only available with Java 24 or
    higher.
    
    Closes https://github.com/eclipse-windowbuilder/windowbuilder/issues/633

--- org.eclipse.wb.os.linux/src_java24/org/eclipse/wb/internal/os/linux/gtk3/GTK3ScreenshotMaker.java
+++ org.eclipse.wb.os.linux/src_java24/org/eclipse/wb/internal/os/linux/gtk3/GTK3ScreenshotMaker.java
@@ -12,22 +12,17 @@
  *******************************************************************************/
 package org.eclipse.wb.internal.os.linux.gtk3;
 
-import org.eclipse.wb.internal.core.utils.reflect.ReflectionUtils;
-import org.eclipse.wb.internal.os.linux.GDK;
-import org.eclipse.wb.internal.os.linux.GtkRuntimeException;
+import org.eclipse.wb.internal.os.linux.Cairo;
+import org.eclipse.wb.internal.os.linux.GTK;
+import org.eclipse.wb.internal.os.linux.GtkAllocation;
 import org.eclipse.wb.internal.os.linux.GtkWidget;
 import org.eclipse.wb.internal.os.linux.ScreenshotMaker;
-import org.eclipse.wb.internal.os.linux.cairo.Cairo;
-import org.eclipse.wb.internal.os.linux.cairo.CairoContext;
-import org.eclipse.wb.internal.os.linux.cairo.CairoFormat;
-import org.eclipse.wb.internal.os.linux.cairo.CairoOperator;
-import org.eclipse.wb.internal.os.linux.cairo.CairoRegion;
-import org.eclipse.wb.internal.os.linux.cairo.CairoSurface;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Device;
+import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
 
+import java.lang.foreign.Arena;
 /**
  * Creates a screenshot of a given widget using the GTK3 API.
  */
@@ -35,69 +30,21 @@ public class GTK3ScreenshotMaker extends ScreenshotMaker {
 
 	@Override
 	protected Image getImageSurface(GtkWidget widget) {
-		GdkWindow window = GTK3.gtk_widget_get_window(widget);
-		if (!GDK3.gdk_window_is_visible(window)) {
-			// don't deal with unmapped windows
-			return null;
-		}
+		try (Arena arena = Arena.ofConfined()) {
+			GtkAllocation allocation = new GtkAllocation(arena);
+			GTK.gtk_widget_get_allocation(widget, allocation);
+			// Prevent allocation warnings
+			GTK3.gtk_widget_get_preferred_size(widget, GtkRequisition.NULL, GtkRequisition.NULL);
+			GTK3.gtk_widget_size_allocate(widget, allocation);
 
-		int width = GDK3.gdk_window_get_width(window);
-		int height = GDK3.gdk_window_get_height(window);
-		// force paint. Note, not all widgets do this completely, known so far is
-		// GtkTreeViewer.
-		GDK3.gdk_window_process_updates(window, true);
-		// take screenshot
-		Image image = createImage(window, width, height);
-		// done
-		return image;
-	}
+			int width = Math.max(1, allocation.width());
+			int height = Math.max(1, allocation.height());
 
-	private Image createImage(GdkWindow sourceWindow, int width, int height) {
-		// Create the Cairo surface on which the snapshot is drawn on
-		CairoSurface targetSurface = Cairo.cairo_image_surface_create(CairoFormat.CAIRO_FORMAT_ARGB32, width, height);
-		CairoContext cr = Cairo.cairo_create(targetSurface);
-		// Get the visible region of the window
-		// Wayland: Trying to take a screenshot of a partially unmapped widget
-		// results in a SIGFAULT.
-		CairoRegion visibleRegion = GDK3.gdk_window_get_visible_region(sourceWindow);
-		// Set the visible region as the clip for the Cairo context
-		GDK.gdk_cairo_region(cr, visibleRegion);
-		Cairo.cairo_clip(cr);
-		// Paint the surface
-		GDK3.gdk_cairo_set_source_window(cr, sourceWindow, 0, 0);
-		Cairo.cairo_set_operator(cr, CairoOperator.CAIRO_OPERATOR_SOURCE);
-		Cairo.cairo_paint(cr);
-		// Cleanup
-		Cairo.cairo_destroy(cr);
-		Cairo.cairo_surface_flush(targetSurface);
-		Cairo.cairo_region_destroy(visibleRegion);
-		return createImage(targetSurface.segment().address());
-	}
-
-	private Image createImage(long imageHandle) {
-		Image image = createImage0(imageHandle);
-		// BUG in SWT: Image instance is not fully initialized
-		// See https://bugs.eclipse.org/bugs/show_bug.cgi?id=382175
-		Image newImage = new Image(null, image.getImageData());
-		image.dispose();
-		return newImage;
-	}
-
-	private Image createImage0(long imageHandle) {
-		try {
-			return (Image) ReflectionUtils.invokeMethod2( //
-					Image.class, //
-					"gtk_new", //
-					Device.class, //
-					int.class, //
-					long.class, //
-					long.class, //
-					null, //
-					SWT.BITMAP, //
-					imageHandle, //
-					0);
-		} catch (Exception e) {
-			throw new GtkRuntimeException(e);
+			Image image = new Image(Display.getCurrent(), width, height);
+			GC gc = new GC(image);
+			GTK3.gtk_widget_draw(widget, Cairo.from(gc));
+			gc.dispose();
+			return image;
 		}
 	}
 
--- org.eclipse.wb.os.linux/src_java24/org/eclipse/wb/internal/os/linux/gtk3/GtkRequisition.java
+++ org.eclipse.wb.os.linux/src_java24/org/eclipse/wb/internal/os/linux/gtk3/GtkRequisition.java
@@ -13,10 +13,25 @@
 package org.eclipse.wb.internal.os.linux.gtk3;
 
 import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
 
 /**
- * A GDK window.
+ * Represents the desired size of a widget.
  */
-public record GdkWindow(MemorySegment segment) {
+public record GtkRequisition(MemorySegment segment) {
+	public static final GtkRequisition NULL = new GtkRequisition(MemorySegment.NULL);
 
+	/**
+	 * @return The widget’s desired width.
+	 */
+	public int width() {
+		return segment.getAtIndex(ValueLayout.JAVA_INT, 0);
+	}
+
+	/**
+	 * @return The widget’s desired height.
+	 */
+	public int height() {
+		return segment.getAtIndex(ValueLayout.JAVA_INT, 0);
+	}
 }
