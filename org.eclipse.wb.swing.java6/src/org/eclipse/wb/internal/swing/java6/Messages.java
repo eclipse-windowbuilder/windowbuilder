d69d592bede47542cc7935413915eea8fbb385bd
Patrick Ziegler <ziegler.patrick@hotmail.de>
2026-07-10 19:21:58 +0200

    Removes several unused classes in Swing Java6 Plugin

--- org.eclipse.wb.swing.java6/src/org/eclipse/wb/internal/swing/java6/Messages.java
+++ org.eclipse.wb.swing.java6/src/org/eclipse/wb/internal/swing/java6/Messages.java
@@ -4,7 +4,6 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.wb.internal.swing.java6.Messages"; //$NON-NLS-1$
-	public static String GroupLayoutParserVisitor_addWithoutArguments;
 	public static String LayoutStyleSupport_unsupportedPlacement;
 	public static String LayoutStyleSupport_unsupportedPosition;
 	static {
