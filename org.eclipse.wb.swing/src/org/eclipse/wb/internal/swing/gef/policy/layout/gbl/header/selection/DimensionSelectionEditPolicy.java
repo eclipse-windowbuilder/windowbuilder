becad7b59e7bb812f278aacaa5f9bb9afcb6926d
Patrick Ziegler <ziegler.patrick@hotmail.de>
2026-09-16 20:07:50 +0200

    Unify feedback-hint logic for `DimensionSelectionEditPolicy`
    The Swing FormLayout, MigLayout and GridBagLayout all use the same
    nearly identical (but sadly broken) logic for positioning the feedback
    figure when one of the grid columns/rows is moved. This component has
    now been moved to the `AbstractDimensionSelectionEditPolicy` so that
    it's only implemented once. Layout-specific logic is executed via the
    abstract `showTextFeedback(...)` and `eraseTextFeedback(...)`.
    
    A test case for the GridBagLayout has been added to avoid regressions in
    the future.

--- org.eclipse.wb.swing/src/org/eclipse/wb/internal/swing/gef/policy/layout/gbl/header/selection/DimensionSelectionEditPolicy.java
+++ org.eclipse.wb.swing/src/org/eclipse/wb/internal/swing/gef/policy/layout/gbl/header/selection/DimensionSelectionEditPolicy.java
@@ -13,8 +13,6 @@
 package org.eclipse.wb.internal.swing.gef.policy.layout.gbl.header.selection;
 
 import org.eclipse.wb.core.gef.figure.TextFeedback;
-import org.eclipse.wb.core.gef.header.AbstractHeaderSelectionEditPolicy;
-import org.eclipse.wb.core.gef.policy.PolicyUtils;
 import org.eclipse.wb.core.gef.policy.layout.grid.IGridInfo;
 import org.eclipse.wb.draw2d.FigureUtils;
 import org.eclipse.wb.draw2d.Layer;
@@ -22,13 +20,12 @@ import org.eclipse.wb.gef.graphical.handles.MoveHandle;
 import org.eclipse.wb.gef.graphical.policies.LayoutEditPolicy;
 import org.eclipse.wb.gef.graphical.policies.SelectionEditPolicy;
 import org.eclipse.wb.internal.swing.gef.policy.layout.gbl.header.edit.DimensionHeaderEditPart;
+import org.eclipse.wb.internal.swing.gef.policy.layout.header.selection.AbstractDimensionSelectionEditPolicy;
 import org.eclipse.wb.internal.swing.model.layout.gbl.AbstractGridBagLayoutInfo;
 import org.eclipse.wb.internal.swing.model.layout.gbl.DimensionInfo;
 
 import org.eclipse.draw2d.ColorConstants;
-import org.eclipse.draw2d.Figure;
 import org.eclipse.draw2d.IFigure;
-import org.eclipse.draw2d.LineBorder;
 import org.eclipse.draw2d.Locator;
 import org.eclipse.draw2d.geometry.Interval;
 import org.eclipse.draw2d.geometry.Point;
@@ -48,9 +45,7 @@ import java.util.List;
  * @author scheglov_ke
  * @coverage swing.gef.policy
  */
-abstract class DimensionSelectionEditPolicy<T extends DimensionInfo>
-extends
-AbstractHeaderSelectionEditPolicy {
+abstract class DimensionSelectionEditPolicy<T extends DimensionInfo> extends AbstractDimensionSelectionEditPolicy {
 
 	////////////////////////////////////////////////////////////////////////////
 	//
@@ -123,9 +118,7 @@ AbstractHeaderSelectionEditPolicy {
 	// Resize
 	//
 	////////////////////////////////////////////////////////////////////////////
-	private IFigure m_lineFeedback;
 	private TextFeedback m_feedback;
-	protected Command m_resizeCommand;
 
 	@Override
 	public boolean understandsRequest(Request request) {
@@ -141,58 +134,21 @@ AbstractHeaderSelectionEditPolicy {
 	}
 
 	@Override
-	public void showSourceFeedback(Request request) {
-		if (!understandsRequest(request)) {
-			return;
+	protected void showTextFeedback(ChangeBoundsRequest changeBoundsRequest, Point feedbackLocation) {
+		Layer feedbackLayer = getMainLayer(LayerConstants.FEEDBACK_LAYER);
+		// add feedback
+		if (m_feedback == null) {
+			m_feedback = new TextFeedback(feedbackLayer);
+			m_feedback.add();
 		}
-		ChangeBoundsRequest changeBoundsRequest = (ChangeBoundsRequest) request;
-		m_resizeCommand = null;
-		// line feedback
-		{
-			// create feedback
-			if (m_lineFeedback == null) {
-				m_lineFeedback = new Figure();
-				LineBorder border = new LineBorder(ColorConstants.red, 2);
-				m_lineFeedback.setBorder(border);
-				addFeedback(m_lineFeedback);
-			}
-			// prepare feedback bounds
-			Rectangle bounds;
-			{
-				IFigure hostFigure = getHostFigure();
-				bounds = changeBoundsRequest.getTransformedRectangle(hostFigure.getBounds());
-				FigureUtils.translateFigureToAbsolute(hostFigure, bounds);
-			}
-			// show feedback
-			m_lineFeedback.setBounds(bounds);
-		}
-		// text feedback
-		{
-			Layer feedbackLayer = getMainLayer(LayerConstants.FEEDBACK_LAYER);
-			// add feedback
-			if (m_feedback == null) {
-				m_feedback = new TextFeedback(feedbackLayer);
-				m_feedback.add();
-			}
-			// set feedback bounds
-			{
-				Point mouseLocation = PolicyUtils.getAbsoluteLocation(getHost(), changeBoundsRequest);
-				Point feedbackLocation = getTextFeedbackLocation(mouseLocation);
-				FigureUtils.translateAbsoluteToFigure(feedbackLayer, feedbackLocation);
-				m_feedback.setLocation(feedbackLocation);
-			}
-			// set text
-			m_feedback.setText(getFeedbackText(changeBoundsRequest));
-		}
+		// set feedback bounds
+		m_feedback.setLocation(feedbackLocation);
+		// set text
+		m_feedback.setText(getFeedbackText(changeBoundsRequest));
 	}
 
 	@Override
-	public void eraseSourceFeedback(Request request) {
-		if (!understandsRequest(request)) {
-			return;
-		}
-		removeFeedback(m_lineFeedback);
-		m_lineFeedback = null;
+	protected void eraseTextFeedback(Request request) {
 		m_feedback.remove();
 		m_feedback = null;
 	}
@@ -214,10 +170,6 @@ AbstractHeaderSelectionEditPolicy {
 	// Resize: abstract feedback
 	//
 	////////////////////////////////////////////////////////////////////////////
-	/**
-	 * @return the location of text feedback (with size hint).
-	 */
-	protected abstract Point getTextFeedbackLocation(Point mouseLocation);
 
 	/**
 	 * @return the text for feedback.
